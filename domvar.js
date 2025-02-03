bb6f3757b6045f3b0c21cd2eef13d89e5c74b859
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-02-03 15:27:12 +0700

    Create domvar.js

---
+++ domvar.js
@@ -0,0 +1,20 @@
+const quick_filter = [
+  "Awareness",
+  "Engagement",
+  "Lead Form",
+  "Message",
+  "Pagelike",
+  "Traffic",
+];
+const goalMapping = {
+  Awareness: ["REACH"],
+  Engagement: ["POST_ENGAGEMENT", "THRUPLAY"],
+  "Lead Form": ["LEAD_GENERATION", "QUALITY_LEAD"],
+  Message: ["REPLIES"],
+  Pagelike: ["OFFSITE_CONVERSIONS", "LINK_CLICKS", "PROFILE_VISIT"],
+  Traffic: ["PAGE_LIKES"],
+};
+
+const accessToken =
+  "EAAQwS9m6tIABO8ZCZCvO4TtPBXcbilAYn3nwZCZB739B8GtMfy2V2uJmgjHMtvsdKS6XMl7YiBuXqg3BxTdh37H7Vv5qYcsZA7IqVYMLqHX3FhQdxD8fSguISa0sDg1INzOfVtUCt8OoNqh0j6PXvu50rZCgMerGZAJ7NAYgLYuTsPw8NvdOEdF5kRX9C0ctu1ka7CS6VcbbXosWnMM"; // Token của bạn
+const adAccountId = "1709508983236012"; // BBH DUYENMAI
