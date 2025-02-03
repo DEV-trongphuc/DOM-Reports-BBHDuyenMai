fce3a8f5d477835b51e2315bd053c36f28b5d655
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-02-03 16:43:42 +0700

    Update domvar.js

--- domvar.js
+++ domvar.js
@@ -7,13 +7,19 @@ const quick_filter = [
   "Traffic",
 ];
 const goalMapping = {
-  Awareness: ["REACH"],
-  Engagement: ["POST_ENGAGEMENT", "THRUPLAY"],
+  Awareness: ["REACH", "AD_RECALL_LIFT", "IMPRESSIONS"],
+  Engagement: ["POST_ENGAGEMENT", "THRUPLAY", "EVENT_RESPONSES"],
   "Lead Form": ["LEAD_GENERATION", "QUALITY_LEAD"],
   Message: ["REPLIES"],
-  Pagelike: ["OFFSITE_CONVERSIONS", "LINK_CLICKS", "PROFILE_VISIT"],
-  Traffic: ["PAGE_LIKES"],
+  Pagelike: ["PAGE_LIKES"],
+  Traffic: [
+    "OFFSITE_CONVERSIONS",
+    "LINK_CLICKS",
+    "PROFILE_VISIT",
+    "LADING_PAGE_VIEWS",
+  ],
 };
+
 
 const accessToken =
   "EAAQwS9m6tIABO8ZCZCvO4TtPBXcbilAYn3nwZCZB739B8GtMfy2V2uJmgjHMtvsdKS6XMl7YiBuXqg3BxTdh37H7Vv5qYcsZA7IqVYMLqHX3FhQdxD8fSguISa0sDg1INzOfVtUCt8OoNqh0j6PXvu50rZCgMerGZAJ7NAYgLYuTsPw8NvdOEdF5kRX9C0ctu1ka7CS6VcbbXosWnMM"; // Token của bạn
