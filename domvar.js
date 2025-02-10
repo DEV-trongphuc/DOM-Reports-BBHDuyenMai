9d1235dce92f5cec73dd40767a74451896b80498
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-02-10 11:37:22 +0700

    Update domvar.js

--- domvar.js
+++ domvar.js
@@ -24,3 +24,5 @@ const goalMapping = {
 const accessToken =
   "EAAQwS9m6tIABO8ZCZCvO4TtPBXcbilAYn3nwZCZB739B8GtMfy2V2uJmgjHMtvsdKS6XMl7YiBuXqg3BxTdh37H7Vv5qYcsZA7IqVYMLqHX3FhQdxD8fSguISa0sDg1INzOfVtUCt8OoNqh0j6PXvu50rZCgMerGZAJ7NAYgLYuTsPw8NvdOEdF5kRX9C0ctu1ka7CS6VcbbXosWnMM"; // Token của bạn
 const adAccountId = "1709508983236012"; // BBH DUYENMAI
+
+let oodo_view = false;
