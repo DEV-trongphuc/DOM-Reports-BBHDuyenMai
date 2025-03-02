0906b1226cc8f45c2fee720a7961ffb5f8f5f7c6
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-03-02 17:24:03 +0700

    Update domvar.js

--- domvar.js
+++ domvar.js
@@ -1,15 +1,18 @@
-const accessToken =
-  "EAAQwS9m6tIABO8ZCZCvO4TtPBXcbilAYn3nwZCZB739B8GtMfy2V2uJmgjHMtvsdKS6XMl7YiBuXqg3BxTdh37H7Vv5qYcsZA7IqVYMLqHX3FhQdxD8fSguISa0sDg1INzOfVtUCt8OoNqh0j6PXvu50rZCgMerGZAJ7NAYgLYuTsPw8NvdOEdF5kRX9C0ctu1ka7CS6VcbbXosWnMM"; // Token của bạn
-
-const quickFilter = [
-  "Awareness",
+let listAccount = [
+  {
+    brand: false,
+    quick: [
+      "Awareness",
   "Engagement",
   "Lead Form",
   "Message",
   "Traffic",
   "Pagelike",
+    ],
+    avatar: "./DOM-img/DM.png",
+    name: "BM Duyên Mai",
+    id: "1709508983236012",
+    access:
+      "EAAQwS9m6tIABO8ZCZCvO4TtPBXcbilAYn3nwZCZB739B8GtMfy2V2uJmgjHMtvsdKS6XMl7YiBuXqg3BxTdh37H7Vv5qYcsZA7IqVYMLqHX3FhQdxD8fSguISa0sDg1INzOfVtUCt8OoNqh0j6PXvu50rZCgMerGZAJ7NAYgLYuTsPw8NvdOEdF5kRX9C0ctu1ka7CS6VcbbXosWnMM",
+  },
 ];
-let isBrand = false;
-const accName = "BM Duyên Mai";
-const accAvatar = "./DOM-img/DM.png";
-const adAccountId = "1709508983236012"; 
